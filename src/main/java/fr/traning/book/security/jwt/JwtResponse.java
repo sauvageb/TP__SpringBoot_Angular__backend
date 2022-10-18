6042b19e2655d87f5bbdbb022e0395e335d28382
sauvageb <sauvageboris.pro@gmail.com>
2022-10-18 19:15:38 +0200

    JWT : configure JWT utility class and dto response

---
+++ src/main/java/fr/traning/book/security/jwt/JwtResponse.java
@@ -0,0 +1,16 @@
+package fr.traning.book.security.jwt;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+
+    private Long userId;
+    private String username;
+    private String token;
+}
